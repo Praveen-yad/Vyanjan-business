df14a3675edb79cc39874ecb5b4e847fb0039c48
Praveen Yadav <108298110+Praveen-yad@users.noreply.github.com>
2023-05-04 17:29:57 +0530

    Url changed

--- src/Url.js
+++ src/Url.js
@@ -1,3 +1,3 @@
-// const Url = 'https://vyanjan-backend.vercel.app/api'
-const Url = 'http://localhost:5000/api'
+const Url = 'https://vyanjan-backend.vercel.app/api'
+// const Url = 'http://localhost:5000/api'
 export default Url
